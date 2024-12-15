6f2db77f7f904d048933fd9569273cbb3bf70e76
Jayansh Devgan <72650671+JayanshDevgan@users.noreply.github.com>
2024-12-15 17:44:01 +0530

    Setting Background Done

--- main.lua
+++ main.lua
@@ -1,5 +1,30 @@
+function love.load()
+    local iconData = love.image.newImageData("images/game.png")
+    love.window.setIcon(iconData)
+
+    math.randomseed(os.time())
+
+    local randomIndex = tostring(math.random(1, 5))
+    local backgroundPath = "images/background/" .. randomIndex .. ".jpeg"
+    
+    -- Print the generated path for debugging
+    print("Trying to load background image from: " .. backgroundPath)
+
+    local imageInfo = love.filesystem.getInfo(backgroundPath)
+
+    if imageInfo then
+        background = love.graphics.newImage(backgroundPath)
+    else
+        error("Image not found: " .. backgroundPath)
+    end
+end
+
 function love.draw()
-  love.graphics.print("Hello, World!", 0, 0)
+  if background then
+      love.graphics.draw(background, 0, -600) 
+  else
+      love.graphics.print("Background image not loaded!", 10, 10)
+  end
 end
 
 function love.update(dt)
