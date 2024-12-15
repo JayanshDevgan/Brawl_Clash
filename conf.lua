17b2d7f3ab8b920f6682d8a24f9c77587bc1a9a6
Jayansh Devgan <72650671+JayanshDevgan@users.noreply.github.com>
2024-12-15 15:54:17 +0530

    Basic Project Setup Done~

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.title = "Brawl Clash"
+    t.window.resizable = true
+
+    t.console = false
+    t.modules.touch = false
+end
---
+++ main.lua
@@ -0,0 +1,10 @@
+function love.draw()
+  love.graphics.print("Hello, World!", 0, 0)
+end
+
+function love.update(dt)
+  
+end
+
+function love.quit()
+end
